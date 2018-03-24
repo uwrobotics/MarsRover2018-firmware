f85381c861b90f68ae10beca48a71c5ef82d04f0
bankya <bankiadebajo@hotmail.com>
2018-03-24 18:00:42 -0400

    Get arm driver to a working state for PWM inputs

--- app/arm_driver/src/main.cpp
+++ app/arm_driver/src/main.cpp
@@ -22,24 +22,31 @@ extern "C" {
 #include "canlib.h"
 #include "pins.h"
 
-#ifdef __cplusplus
-}
-#endif
-
 //#define ABSOLUTE
 #define INCREMENTAL
+#define NUM_MOTORS 5
 
 Serial pc(USBTX, USBRX);
-uint32_t enc_ID[5] = {300, 301, 302, 303, 304};
-uint32_t motor_ID[5] = {400, 401, 402, 403, 404};
+uint32_t enc_ID[NUM_MOTORS] = {300, 301, 302, 303, 304};
+uint32_t motor_ID[NUM_MOTORS] = {400, 401, 402, 403, 404};
 
 uint32_t t_on, t_off;
 uint16_t width, position;
 float duty_cycle;
-volatile uint16_t data[5];
+volatile uint16_t data[NUM_MOTORS];
 
-volatile uint32_t pwm_duty[5];
+volatile uint32_t pwm_duty[NUM_MOTORS];
 volatile uint8_t data_ready = 0;
+
+const PinName pwm_pins[NUM_MOTORS] = {PB_13, PB_15, PC_7, PC_9, PA_9}; // PWM_6 is not included (PA_11)
+const PinName dir_pins[NUM_MOTORS] = {PB_14, PC_6, PC_8, PA_8, PA_10}; // DIR_6 is not included (PA_12)
+
+PwmOut *pwm_out[NUM_MOTORS];
+DigitalOut *dir_out[NUM_MOTORS];
+
+float freq = 50.0; // PWM Frequency [Hz]
+float period = 1/freq;
+DigitalOut led1(PC_1);
 
 //QEI library used to read from incremental encoders.
 //To-Do : define pins A, B and I depending on hardware setup 
@@ -47,16 +54,26 @@ QEI wheel_1 (A0, A1, A2, 48);
 QEI wheel_2 (A0, A1, A2, 48);
 QEI wheel_3 (A0, A1, A2, 48);
 
-PwmIn abs_enc_1(A4);
-PwmIn abs_enc_2(A5);
+// PwmIn abs_enc_1(A4);
+// PwmIn abs_enc_2(A5);
 
+/*
+ *	Initialize pins
+ */
+void initPins() {
+	for (int i = 0; i < NUM_MOTORS; i++) {
+		pwm_out[i] = new PwmOut(pwm_pins[i]);
+		dir_out[i] = new DigitalOut(dir_pins[i]);
+
+		// Add Encoder Pin Initialization
+	}
+}
 
 /*
  *  Generates a PWM signal with a duty cycle of pwm_duty/256
  */
-void pwm_gen(uint32_t pwm_duty)
+void pwmGen(float pwm_duty, PwmOut pwm_pin)
 {
-    float period = 1/freq;  
     pwm_pin.period(period);
     if (pwm_duty <= 255)
     {
@@ -67,9 +84,9 @@ void pwm_gen(uint32_t pwm_duty)
 /*
  *  Controls the motor speed. Direction = 1 is clockwise
  */
-void motor_control(int16_t speed)
+void motorControl(int16_t speed, PwmOut pwm_pin, DigitalOut dir_pin)
 {
-    pwm_gen(abs(speed));
+    pwmGen(abs(speed), pwm_pin);
     dir_pin = (speed <= 0);
 }
 
@@ -78,85 +95,89 @@ void motor_control(int16_t speed)
  */
 void CANLIB_Rx_OnMessageReceived(void)
 {
-
-    switch(CANLIB_Rx_GetSenderID())
-    {
-        // Expect frame format to be:
-        // Bytes 0-3: Azimuth axis PWM input
-        // Byte 4-7: Inclination axis PWM input
-        case motor_ID[0]:
-            pwm_duty[0] = CANLIB_Rx_GetAsFloat(motor_ID[0]);
-            data_ready = 1;
-            break;
-
-
-    pwm_duty = CANLIB_Rx_GetAsUint(CANLIB_INDEX_0);
+	uint16_t sender_ID = CANLIB_Rx_GetSenderID();
+    
+	for (int i = 0; i < NUM_MOTORS; i++) {
+		if (motor_ID[i] == sender_ID) {
+			pwm_duty[i] = CANLIB_Rx_GetAsFloat(CANLIB_INDEX_0) * 255.0;
+			data_ready = 1;
+			led1 = 1;
+			break;
+		}
+	}
 }
 
+/*
+ *  Handle any errors that occur
+ */
+void errorHandler() {
+
+}
 
 int main(){
+	initPins();
 
-	if (CANLIB_Init(enc_ID, CANLIB_LOOPBACK_OFF) != 0){
-		pc.printf("init failed \r\n");
+	// Initialize with the first encoder value. Value updated before sending each time
+	if (CANLIB_Init(enc_ID[0], CANLIB_LOOPBACK_OFF) != 0) {
+		errorHandler();
 	}
 
-	if (CANLIB_AddFilter(motor_ID[0]) != 0)
-    {
-        Error_Handler();
+	for (int i = 0; i < NUM_MOTORS; i++) {
+		if (CANLIB_AddFilter(motor_ID[i]) != 0) {
+			errorHandler();
+		}
 	}
 
-	uint8_t count;
-	count =0;
-	void reset();
+	uint8_t count = 0;
 
-	while (1){
+	// void reset();
 
-		count ++;
+	while (true){
+
+		count++;
 		//Read Data from Encoder
-		data[0] = wheel_1.getCurrentState();
-		data[1] = wheel_1.getCurrentState();
-		data[2] = wheel_1.getCurrentState();
+		// data[0] = wheel_1.getCurrentState();
+		// data[1] = wheel_1.getCurrentState();
+		// data[2] = wheel_1.getCurrentState();
 
-		width = int(abs_enc_1.dutycycle*4098) -1;
-		//width = ((t_on *4098)/(t_on + t_off)) -1;
-		if(width<=4094)
-			position = width;
-		if (width == 4096)
-			position = 4095;
-
-		data[3] = position;
-
-		width = int(abs_enc_2.dutycycle*4098) -1;
-		//width = ((t_on *4098)/(t_on + t_off)) -1;
-		if(width<=4094)
-			position = width;
-		if (width == 4096)
-			position = 4095;
-
-		data[4] = position;
+		// width = int(abs_enc_1.dutycycle*4098) -1;
 		
+		// //width = ((t_on *4098)/(t_on + t_off)) -1;
+		// if(width <= 4094)
+		// 	position = width;
+		// if (width == 4096)
+		// 	position = 4095;
 
+		// data[3] = position;
+
+		// width = int(abs_enc_2.dutycycle*4098) -1;
+		// //width = ((t_on *4098)/(t_on + t_off)) -1;
+		// if(width <= 4094)
+		// 	position = width;
+		// if (width == 4096)
+		// 	position = 4095;
+
+		// data[4] = position;
+		
 		//Write data to CAN BUS
 		if (count > 100){
-			//pc.printf("Test data %d \n", data);
 			for (int i =0; i< 5; i++){
-				CANLIB_ChangeID(Encoder_ID[i]);
-				CANLIB_Tx_SendData(data[i]);				
+				CANLIB_ChangeID(enc_ID[i]);
+				// CANLIB_Tx_SendData(data[i]);				
 			}
 
 			count = 0;
 		}
 
-		if (data_ready){
-			pwm_gen();
+		if (data_ready) {
+			for (int i = 0; i < NUM_MOTORS; i++) {
+				motorControl(pwm_duty[i], *pwm_out[i], *dir_out[i]);
+			}
+			data_ready = 0;
 		}
-
 	}
-
 }
-/*
+
 #ifdef __cplusplus
 }
 #endif
-
-*/
