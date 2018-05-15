9dd2dd11cbae5b17ab0f43a017a76157856eea0a
Kevin Huo <khbhuo@users.noreply.github.com>
2018-05-14 21:22:08 -0700

    updated science module pwm code

--- app/science_module/src/main.cpp
+++ app/science_module/src/main.cpp
@@ -20,45 +20,48 @@ extern "C" {
 #include "canlib.h"
 #include "pins.h"
 
-#define NUM_MODULES     4
+#define NUM_MODULES 4
 
 Serial pc(USBTX, USBRX);
+I2C i2c(I2C_SDA, I2C_SCL);
 
-// TODO: make enum or something
-static uint32_t elevator_filter_ID  = 420;
-static uint32_t drill_filter_ID     = 421;
-static uint32_t temp_filter_ID      = 422;
-static uint32_t flap_filter_ID      = 423;
+enum module_ids{
+    science_module_pwm_filter   = 400,
+    elevator_pwm_filter_ID      = 420,
+    drill_pwm_filter_ID         = 421,
+    temp_pwm_filter_ID          = 422,
+    flap_pwm_filter_ID          = 423,
 
-static uint32_t elevator_pwm_ID     = 520;
-static uint32_t drill_pwm_ID        = 521;
-static uint32_t temp_pwm_ID         = 522;  
-static uint32_t flap_pwm_ID         = 523;
+    elevator_pwm_ID             = 424,
+    drill_pwm_ID                = 425,
+    temp_pwm_ID                 = 426,
+    flap_pwm_ID                 = 427,
+};
 
-static bool can_msg_received = false;
+static bool can_write_pwm = false;
 static uint16_t can_id;
 static float pwm_duty;
 
 // TODO: fix the pin numbers; current ones are guesses
-PwmOut* elevator_pwm_pin = new PwmOut(PB_13);
-PwmOut* drill_pwm_pin = new PwmOut(PB_14);
-PwmOut* temp_pwm_pin = new PwmOut(PB_15);
-PwmOut* flap_pwm_pin = new PwmOut(PB_15);
+PwmOut* elevator_pwm_pin        = new PwmOut(PB_13);
+PwmOut* drill_pwm_pin           = new PwmOut(PB_14);
+PwmOut* temp_pwm_pin            = new PwmOut(PB_15);
+PwmOut* flap_pwm_pin            = new PwmOut(PB_15);
 
-DigitalOut* elevator_dir_pin = new DigitalOut(PC_6);
-DigitalOut* drill_dir_pin = new DigitalOut(PC_7);
-DigitalOut* temp_dir_pin = new DigitalOut(PC_8);
-DigitalOut* flap_dir_pin = new DigitalOut(PC_9);
+DigitalOut* elevator_dir_pin    = new DigitalOut(PC_6);
+DigitalOut* drill_dir_pin       = new DigitalOut(PC_7);
+DigitalOut* temp_dir_pin        = new DigitalOut(PC_8);
+DigitalOut* flap_dir_pin        = new DigitalOut(PC_9);
 
 static PwmOut* pwm_out_vector[NUM_MODULES] = {elevator_pwm_pin,
-                                                drill_pwm_pin,
-                                                temp_pwm_pin,
-                                                flap_pwm_pin};
+                                              drill_pwm_pin,
+                                              temp_pwm_pin,
+                                              flap_pwm_pin};
 
 static DigitalOut* digital_out_vector[NUM_MODULES] = {elevator_dir_pin,
-                                                drill_dir_pin,
-                                                temp_dir_pin,
-                                                flap_dir_pin};
+                                                      drill_dir_pin,
+                                                      temp_dir_pin,
+                                                      flap_dir_pin};
 
 /*
  *  Controls for each instrument.
@@ -75,9 +78,16 @@ void control_modules(float pwm_duty, uint16_t module_id)
  */
 void CANLIB_Rx_OnMessageReceived(void)
 {
-    pwm_duty = CANLIB_Rx_GetAsFloat(CANLIB_INDEX_0);
     can_id = CANLIB_Rx_GetSenderID();
-    can_msg_received = true;
+    switch(can_id){
+        case elevator_pwm_filter_ID:
+        case drill_pwm_filter_ID:
+        case temp_pwm_filter_ID:
+        case flap_pwm_filter_ID:
+            pwm_duty = CANLIB_Rx_GetAsFloat(CANLIB_INDEX_0);
+            can_write_pwm = true;
+            break;
+    }
 }
 
 int main()
@@ -91,18 +101,22 @@ int main()
         {
             pc.printf("CAN Initialization Failed on PWM ID: %u \r\n", elevator_pwm_ID + i);
         }
-        if (CANLIB_AddFilter(elevator_filter_ID + i) != 0)
+        if (CANLIB_AddFilter(elevator_pwm_filter_ID + i) != 0)
         {
-            pc.printf("CAN Add Filter Failed on Filter: %u \r\n", elevator_filter_ID + i);
+            pc.printf("CAN Add Filter Failed on Filter: %u \r\n", elevator_pwm_filter_ID + i);
         } 
     }
 
     while(1)
     {
-        if(can_msg_received)
+        if(can_write_pwm)
         {
+            control_modules(pwm_duty, can_id % science_module_pwm_filter);
+            can_write_pwm = false;
+        }
+        {
+            control_modules(pwm_duty, can_id % module_filter);
             can_msg_received = false;
-            control_modules(pwm_duty, can_id % 400);
         }
     }
 }
